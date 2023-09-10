47363e37129564f3f3310988a66a0d265e42341f
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-10 14:07:43 +0530

    upload to cloud

---
+++ src/app/api/auth/upload/route.js
@@ -0,0 +1,36 @@
+import upload from '@/app/services/cloudinary';
+import { NextResponse, NextRequest } from 'next/server';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(request) {
+  // const formData = await form.parse(request);
+
+  const formData = await request.formData();
+  const name = await formData.get('name');
+  const image = await formData.get('image');
+
+  try {
+    const uploadData = await upload(image, name);
+    let json_response = {
+      status: true,
+      data: uploadData,
+    };
+    return NextResponse.json(json_response);
+  } catch (error) {
+    let json_response = {
+      status: false,
+      message: 'Some error occured',
+    };
+    return NextResponse.json(json_response, {
+      status: 500,
+      headers: {
+        'Access-Control-Allow-Methods': 'POST',
+      },
+    });
+  }
+}
---
+++ src/app/services/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { NextResponse } from 'next/server';
+import streamifier from 'streamifier';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+export default async function upload(img, name) {
+  var uploadData = {};
+  const bytes = await img.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  const uploadFn = () => {
+    return new Promise((resolve, reject) => {
+      let cld_upload_stream = cloudinary.uploader.upload_stream(
+        {
+          folder: 'images',
+        },
+        (error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        }
+      );
+      streamifier.createReadStream(buffer).pipe(cld_upload_stream);
+    });
+  };
+
+  const data = await uploadFn();
+
+  return data?.secure_url;
+}
