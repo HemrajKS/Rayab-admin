62f8e1327d2e7903527a940c72cc8019831c51cb
HemrajKShettigar <hemaraja.shettigar@lyveglobal.com>
2023-09-12 14:55:20 +0530

    changes done

---
+++ src/app/api/auth/verify/route.js
@@ -0,0 +1,72 @@
+import { getErrorResponse } from "@/lib/helpers";
+import connectDB from "@/lib/mongodb";
+import UserModel from "@/models/user";
+import Verify from "@/models/verification";
+import { compare } from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  const body = await req.json();
+  try {
+    await connectDB();
+    const [user] = await Verify.find({
+      email: body.email,
+      _id: body.id,
+    });
+
+    if (body.email && body.id && body.otp) {
+      if (JSON.stringify(user) === "[]" || !user) {
+        return getErrorResponse(404, "Not Registered or OTP expired");
+      } else {
+        if (body.otp) {
+          if (!(await compare(body.otp, user.otp))) {
+            return getErrorResponse(401, "Invalid OTP");
+          } else {
+            try {
+              const user = await UserModel.findOneAndUpdate(
+                {
+                  _id: body.id,
+                },
+                { isActive: true },
+                { new: true }
+              );
+              const sanitizedNewUser = { ...user.toObject() };
+              delete sanitizedNewUser.password;
+
+              return new NextResponse(
+                JSON.stringify({
+                  status: "success",
+                  message:
+                    "Congratuation you have been successfully registered",
+                  data: sanitizedNewUser,
+                }),
+                {
+                  status: 200,
+                  headers: { "Content-Type": "application/json" },
+                }
+              );
+            } catch (error) {
+              return getErrorResponse(500, "Error in fetching the data");
+            }
+          }
+        } else {
+          return getErrorResponse(404, "Please enter OTP");
+        }
+      }
+    } else {
+      return getErrorResponse(404, "Some fields are missing");
+    }
+
+    const response = new NextResponse(
+      JSON.stringify({
+        status: true,
+      }),
+      {
+        status: 200,
+      }
+    );
+    return response;
+  } catch (error) {
+    return getErrorResponse(500, error.message);
+  }
+}
--- src/models/verification.js
+++ src/models/verification.js
@@ -14,6 +14,7 @@ const verficationSchema = new mongoose.Schema(
     otp: {
       type: String,
       required: true,
+      index: true
     },
     createdAt: {
       type: Date,
