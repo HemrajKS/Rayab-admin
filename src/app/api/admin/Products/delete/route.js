2d41c984ce1f3c429115561e41e118fa65076cf0
HemrajKShettigar <hemaraja.shettigar@lyveglobal.com>
2023-09-13 13:03:08 +0530

    delete product

---
+++ src/app/api/admin/Products/delete/route.js
@@ -0,0 +1,44 @@
+import { getErrorResponse } from "@/lib/helpers";
+import connectDB from "@/lib/mongodb";
+import Product from "@/models/product";
+import UserModel from "@/models/user";
+import { NextResponse } from "next/server";
+
+export async function DELETE(req) {
+  try {
+    await connectDB();
+    const body = await req.json();
+
+    const userId = req.headers.get("x-user-id");
+    const user = await UserModel.findOne({ _id: userId });
+    if (user.isAdmin) {
+      try {
+        const deleteProduct = await Product.findOneAndDelete(
+          { _id: body.id },
+        );
+        let json_response = {
+          status: true,
+          message: "Product deleted successfully",
+          data: deleteProduct,
+        };
+        return NextResponse.json(json_response);
+      } catch (error) {
+        return getErrorResponse(400, "Could not delete product");
+      }
+    } else {
+      return getErrorResponse(403, "Only Admins can delete products.");
+    }
+  } catch (error) {
+    let json_response = {
+      status: false,
+      results: "some error occured",
+      error: error,
+    };
+    return NextResponse.json(json_response, {
+      status: 500,
+      headers: {
+        "Access-Control-Allow-Methods": "DELETE",
+      },
+    });
+  }
+}
