8dab03d0e734e6f249b8b4bb171bcaa32dc3b44f
HemrajKShettigar <hemaraja.shettigar@lyveglobal.com>
2023-09-14 17:32:09 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+"use client"
+import { useAuth } from "@/contexts/AuthContext";
 import { useSession } from "next-auth/react";
 
 
 export default function Home() {
-  
+  const { isLoggedIn, login, logout } = useAuth();
   return (
     <>
     <h1>Welcome to home page</h1>
