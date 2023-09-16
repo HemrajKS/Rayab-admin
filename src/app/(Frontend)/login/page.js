b9bc2eb1690a81c4e726bf7fe65f00d713aef12f
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-16 06:49:24 +0530

    fixed i phone issues

--- src/app/(Frontend)/login/page.js
+++ src/app/(Frontend)/login/page.js
@@ -35,7 +35,7 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center absolute top-0 bottom-0 left-0 right-0">
-      <div className="sm:relative absolute sm:top-0 sm:right-0 sm:left-0 sm:right-0 flex flex-col sm:rounded-[12px] p-[25px] sm:min-h-[250px] sm:min-w-[250px] sm:max-h-[520px] sm:max-w-[520px] h-full w-full items-center justify-center bg-white min-w-500 sm:rounded-2.5 shadow-md ">
+      <div className="sm:relative absolute sm:top-0 sm:right-0 sm:left-0 sm:right-0 flex flex-col sm:rounded-[12px] p-[25px] sm:min-h-[250px] sm:min-w-[250px] sm:max-h-[520px] sm:max-w-[520px] h-full w-full items-center justify-center bg-white min-w-500 sm:rounded-2.5 shadow-md " style={{height:"100dvh"}}>
         <Image
           src={require('/src/assets/Images/Rayab.png')}
           alt="Logo"
