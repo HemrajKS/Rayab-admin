d402e1fdf7b4f89a534dfad83571e7488ea98853
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-19 21:41:44 +0530

    changes

--- src/middleware.js
+++ src/middleware.js
@@ -1,36 +1,36 @@
-import { NextRequest, NextResponse } from "next/server";
-import { verifyJWT } from "./lib/token";
-import { getErrorResponse } from "./lib/helpers";
-import { protectedRoutes } from "./app/constants/constants";
+import { NextRequest, NextResponse } from 'next/server';
+import { verifyJWT } from './lib/token';
+import { getErrorResponse } from './lib/helpers';
+import { protectedRoutes } from './app/constants/constants';
 
 export async function middleware(req) {
   let token;
 
   if (
-    req.nextUrl.pathname.startsWith("/api/admin") ||
+    req.nextUrl.pathname.startsWith('/api/admin') ||
     protectedRoutes.includes(req.nextUrl.pathname)
   ) {
-    if (req.cookies.has("token")) {
-      token = req.cookies.get("token")?.value;
-    } else if (req.headers.get("Authorization")?.startsWith("Bearer ")) {
-      token = req.headers.get("Authorization")?.substring(7);
+    if (req.cookies.has('token')) {
+      token = req.cookies.get('token')?.value;
+    } else if (req.headers.get('Authorization')?.startsWith('Bearer ')) {
+      token = req.headers.get('Authorization')?.substring(7);
     } else {
       return getErrorResponse(
         401,
-        "You are not loggen in, Please log in to proceed"
+        'You are not loggen in, Please log in to proceed'
       );
     }
 
     try {
-      const response =NextResponse.next()
+      const response = NextResponse.next();
       if (token) {
         const { sub, name, email } = await verifyJWT(token);
         req.user = { id: sub, name, email };
-        response.headers.set("X-USER-ID", sub);
+        response.headers.set('X-USER-ID', sub);
         return response;
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
       return getErrorResponse(401, "Token is invalid or user doesn't exists");
     }
   } else {
@@ -39,8 +39,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-  matcher: [
-    
-    '/((?!_next/static|_next/image|favicon.ico).*)',
-  ],
-}
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+};
