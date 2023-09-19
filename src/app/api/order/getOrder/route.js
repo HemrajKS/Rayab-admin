f51c1ecfeec84934d044b8f7e545a12dd24440e6
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-19 20:36:17 +0530

    sec

--- src/app/api/order/getOrder/route.js
+++ src/app/api/order/getOrder/route.js
@@ -1,33 +1,38 @@
-import { getErrorResponse } from "@/lib/helpers";
-import connectDB from "@/lib/mongodb";
-import Order from "@/models/order";
-import User from "@/models/user";
-import { NextResponse, NextRequest } from "next/server";
+import { getErrorResponse } from '@/lib/helpers';
+import connectDB from '@/lib/mongodb';
+import Order from '@/models/order';
+import User from '@/models/user';
+import { NextResponse, NextRequest } from 'next/server';
+import { headers } from 'next/headers';
 
 export async function GET(req) {
+  const headersList = headers();
+  const userId = headersList.get('x-user-id');
+
   try {
     await connectDB();
-    const userId = req.headers.get("x-user-id");
+    // const userId = req.headers.get("x-user-id");
+
     const user = await User.findOne({ _id: userId });
-    const searchQuery = req.nextUrl.searchParams.get("search") || "";
-    const statusFilter = req.nextUrl.searchParams.get("status") || "";
-    const id = req.nextUrl.searchParams.get("id") || null;
+    const searchQuery = req.nextUrl.searchParams.get('search') || '';
+    const statusFilter = req.nextUrl.searchParams.get('status') || '';
+    const id = req.nextUrl.searchParams.get('id') || null;
 
     if (user && user.isAdmin) {
       let searchCriteria = {
         $or: [
-          { name: { $regex: searchQuery, $options: "i" } },
-          { email: { $regex: searchQuery, $options: "i" } },
-          { phone1: { $regex: searchQuery, $options: "i" } },
-          { phone2: { $regex: searchQuery, $options: "i" } },
+          { name: { $regex: searchQuery, $options: 'i' } },
+          { email: { $regex: searchQuery, $options: 'i' } },
+          { phone1: { $regex: searchQuery, $options: 'i' } },
+          { phone2: { $regex: searchQuery, $options: 'i' } },
         ],
       };
       const orderCount = !id && (await Order.countDocuments(searchCriteria));
-      const skip = JSON.parse(req.nextUrl.searchParams.get("skip"))
-        ? JSON.parse(req.nextUrl.searchParams.get("skip"))
+      const skip = JSON.parse(req.nextUrl.searchParams.get('skip'))
+        ? JSON.parse(req.nextUrl.searchParams.get('skip'))
         : 0;
-      const limit = JSON.parse(req.nextUrl.searchParams.get("limit"))
-        ? JSON.parse(req.nextUrl.searchParams.get("limit"))
+      const limit = JSON.parse(req.nextUrl.searchParams.get('limit'))
+        ? JSON.parse(req.nextUrl.searchParams.get('limit'))
         : orderCount;
 
       if (statusFilter) {
@@ -45,21 +50,21 @@ export async function GET(req) {
         };
         return NextResponse.json(json_response);
       } else {
-        return getErrorResponse(404, "Order not found");
+        return getErrorResponse(404, 'Order not found');
       }
     } else {
-      return getErrorResponse(403, "Please login as admin");
+      return getErrorResponse(403, 'Please login as admin');
     }
   } catch (error) {
     let json_response = {
       status: false,
-      results: "some error occured",
+      results: 'some error occured',
       error: error,
     };
     return NextResponse.json(json_response, {
       status: 500,
       headers: {
-        "Access-Control-Allow-Methods": "GET",
+        'Access-Control-Allow-Methods': 'GET',
       },
     });
   }
