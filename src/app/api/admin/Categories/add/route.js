0ca0405457c11c50e009d3f71819c28755e08b8d
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-19 22:11:05 +0530

    changes

--- src/app/api/admin/Categories/add/route.js
+++ src/app/api/admin/Categories/add/route.js
@@ -1,52 +1,59 @@
-import { getErrorResponse } from "@/lib/helpers";
-import connectDB from "@/lib/mongodb";
-import Category from "@/models/category";
-import UserModel from "@/models/user";
-import { NextResponse } from "next/server";
+import { getErrorResponse } from '@/lib/helpers';
+import connectDB from '@/lib/mongodb';
+import Category from '@/models/category';
+import UserModel from '@/models/user';
+import { NextResponse } from 'next/server';
 
 export async function POST(req) {
   try {
     await connectDB();
     const body = await req.json();
-    const userId = req.headers.get("x-user-id");
+    const userId = req.headers.get('x-user-id');
     const catObj = { ...body, addedBy: userId };
 
     const user = await UserModel.findOne({ _id: userId });
-    if (user.isAdmin) {
-      const newCat = new Category(catObj);
-      const savedCat = await newCat.save();
-      let json_response = {
-        status: true,
-        message: "Category added successfully",
-        data: savedCat,
-      };
-      return NextResponse.json(json_response);
-    } else {
-      return getErrorResponse(403, "Only Admins can add category.");
-    }
+    // if (user.isAdmin) {
+    //   const newCat = new Category(catObj);
+    //   const savedCat = await newCat.save();
+    //   let json_response = {
+    //     status: true,
+    //     message: "Category added successfully",
+    //     data: savedCat,
+    //   };
+    //   return NextResponse.json(json_response);
+    // } else {
+    //   return getErrorResponse(403, "Only Admins can add category.");
+    // }
+
+    let json_response = {
+      status: true,
+      message: 'Category added successfully',
+      data: { user, userId: userId },
+    };
+    return NextResponse.json(json_response);
   } catch (error) {
     if (error.code === 11000) {
       let json_response = {
         status: false,
-        results: "Duplicate Entry",
+        results: 'Duplicate Entry',
         error: error,
       };
       return NextResponse.json(json_response, {
         status: 403,
         headers: {
-          "Access-Control-Allow-Methods": "POST",
+          'Access-Control-Allow-Methods': 'POST',
         },
       });
     }
     let json_response = {
       status: false,
-      results: "Enter all required fields",
+      results: 'Enter all required fields',
       error: error.message,
     };
     return NextResponse.json(json_response, {
       status: 500,
       headers: {
-        "Access-Control-Allow-Methods": "POST",
+        'Access-Control-Allow-Methods': 'POST',
       },
     });
   }
