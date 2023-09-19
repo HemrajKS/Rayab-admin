c066953271784d0d7469da5d2c966f0892c392ca
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-19 14:44:28 +0530

    sec

---
+++ src/app/api/admin/Users/route.js
@@ -0,0 +1,38 @@
+import { getErrorResponse } from '@/lib/helpers';
+import connectDB from '@/lib/mongodb';
+import UserModel from '@/models/user';
+import { NextResponse } from 'next/server';
+
+export async function GET(req) {
+  try {
+    await connectDB();
+    const userId = req.headers.get('x-user-id');
+
+    const user = await UserModel.findOne({ _id: userId });
+    if (user.isAdmin) {
+      const users = await UserModel.find({}).select('-password');
+
+      let json_response = {
+        status: true,
+        message: 'User data',
+        data: users,
+      };
+      return NextResponse.json(json_response);
+    } else {
+      return getErrorResponse(403, 'Only Admins can get user data');
+    }
+  } catch (error) {
+    console.log(error);
+    let json_response = {
+      status: false,
+      results: 'some error occured',
+      error: error,
+    };
+    return NextResponse.json(json_response, {
+      status: 500,
+      headers: {
+        'Access-Control-Allow-Methods': 'GET',
+      },
+    });
+  }
+}
