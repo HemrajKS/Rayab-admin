44d7818991f7c30056163faee7426371c984146f
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-19 19:33:19 +0530

    changes

--- src/middleware.js
+++ src/middleware.js
@@ -37,3 +37,10 @@ export async function middleware(req) {
     return NextResponse.next();
   }
 }
+
+export const config = {
+  matcher: [
+    
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
+}
