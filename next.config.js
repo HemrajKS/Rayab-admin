99baf8c07acfe36ce66ecf96626306e5660cf67d
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-19 19:57:17 +0530

    sec

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  runtime: 'edge',
-  unstable_allowDynamic: [
-    '/lib/utilities.js',
-    '/node_modules/function-bind/**',
-    '/src/app/constants/constants.js',
-    '/node_modules/@mui/**',
-  ],
   images: {
     domains: ['images.unsplash.com', 'res.cloudinary.com'],
   },
