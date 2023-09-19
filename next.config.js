17015768bef533bbb1bbb3a44f74e06cc71da75d
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-19 05:43:40 +0530

    sec

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  optimizeCss: false,
-  enableBabelRuntime: true,
   images: {
     domains: ['images.unsplash.com', 'res.cloudinary.com'],
   },
