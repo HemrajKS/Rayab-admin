43c73a7957136c96e683057f916aa9c9eb355976
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-23 21:05:25 +0530

    sec

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -9,7 +9,7 @@ export async function GET(req) {
   // const userId = req.headers.get('X-User-Id');
 
   let token = req.cookies.get('token')?.value;
-  const userId = (await verifyJWT(token)).sub;
+  const userId = (await verifyJWT(token)).sub.toString();
 
   try {
     await connectDB();
