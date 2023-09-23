65308bfcf517dac7c57af52b00200f2ff274c933
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-23 21:19:47 +0530

    sec

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -8,12 +8,11 @@ import { NextResponse } from 'next/server';
 export async function GET(req) {
   // const userId = req.headers.get('X-User-Id');
 
-  let token = req.cookies.get('token')?.value;
-  const { sub } = await verifyJWT(token);
-
-  const userId = sub.toString();
-
   try {
+    let token = req.cookies.get('token')?.value;
+    const { sub } = await verifyJWT(token);
+
+    const userId = sub;
     await connectDB();
     if (userId) {
       const profile = await User.findOne({ _id: userId }).select('-password');
