545d674964a4c606228dad71369349979a37f827
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-24 08:51:00 +0530

    sec

--- src/app/api/admin/Categories/delete/route.js
+++ src/app/api/admin/Categories/delete/route.js
@@ -6,19 +6,20 @@ import UserModel from '@/models/user';
 import { NextResponse } from 'next/server';
 
 export async function DELETE(req) {
-  await connectDB();
-  const body = await req.json();
   try {
+    await connectDB();
+    const body = await req.json();
     // const userId = await req.headers.get('X-User-Id');
 
     let token = req.cookies.get('token')?.value;
     const userId = (await verifyJWT(token)).sub;
     const user = await UserModel.findOne({ _id: userId });
     if (user.isAdmin) {
-      await Category.deleteOne({ _id: body.id });
+      // await Category.deleteOne({ _id: body.id });
       let json_response = {
         status: true,
         message: 'Category deleted successfully',
+        body: body,
       };
       return NextResponse.json(json_response);
     } else {
@@ -26,8 +27,9 @@ export async function DELETE(req) {
     }
   } catch (error) {
     return getErrorResponse(403, {
-      message: 'Only Admins can delete category.',
+      message: 'Some error',
       error: error,
+      body: body,
     });
   }
 }
