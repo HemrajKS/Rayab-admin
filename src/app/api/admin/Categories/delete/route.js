5a3ecaa9eebd1bd74440c7243972789514fc08b3
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-09-24 08:59:20 +0530

    sec

--- src/app/api/admin/Categories/delete/route.js
+++ src/app/api/admin/Categories/delete/route.js
@@ -8,23 +8,24 @@ import { NextResponse } from 'next/server';
 export async function DELETE(req) {
   try {
     await connectDB();
-    const body = await req.json();
+    const body = req;
     // const userId = await req.headers.get('X-User-Id');
 
-    let token = req.cookies.get('token')?.value;
-    const userId = (await verifyJWT(token)).sub;
-    const user = await UserModel.findOne({ _id: userId });
-    if (user.isAdmin) {
-      // await Category.deleteOne({ _id: body.id });
-      let json_response = {
-        status: true,
-        message: 'Category deleted successfully',
-        body: body,
-      };
-      return NextResponse.json(json_response);
-    } else {
-      return getErrorResponse(403, 'Only Admins can delete category.');
-    }
+    // let token = req.cookies.get('token')?.value;
+    // const userId = (await verifyJWT(token)).sub;
+    // const user = await UserModel.findOne({ _id: userId });
+    // if (user.isAdmin) {
+    // await Category.deleteOne({ _id: body.id });
+    let json_response = {
+      status: true,
+      message: 'Category deleted successfully',
+      body: body,
+    };
+    return NextResponse.json(json_response);
+    // }
+    // else {
+    //   return getErrorResponse(403, 'Only Admins can delete category.');
+    // }
   } catch (error) {
     return getErrorResponse(403, {
       message: 'Some error',
