9fd6f3ce543b4bcad4fbfbc2e4cafc657e6068d0
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-10-06 21:31:42 +0530

    sec

--- src/app/api/authentication/login/route.js
+++ src/app/api/authentication/login/route.js
@@ -1,9 +1,9 @@
-import { getEnvVariable, getErrorResponse } from "@/lib/helpers";
-import connectDB from "@/lib/mongodb";
-import { signJWT } from "@/lib/token";
-import { compare } from "bcryptjs";
-import UserModel from "@/models/user";
-import { NextRequest, NextResponse } from "next/server";
+import { getEnvVariable, getErrorResponse } from '@/lib/helpers';
+import connectDB from '@/lib/mongodb';
+import { signJWT } from '@/lib/token';
+import { compare } from 'bcryptjs';
+import UserModel from '@/models/user';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req, res) {
   const body = await req.json();
@@ -20,10 +20,10 @@ export async function POST(req, res) {
       !(await compare(body.password, user.password)) ||
       !user.isActive
     ) {
-      return getErrorResponse(401, "Invalid email or password");
+      return getErrorResponse(401, 'Invalid email or password');
     }
 
-    const JWT_EXPIRES_IN = getEnvVariable("JWT_EXPIRES_IN");
+    const JWT_EXPIRES_IN = getEnvVariable('JWT_EXPIRES_IN');
 
     const token = await signJWT(
       { sub: user._id, name: user.username, password: user.password },
@@ -32,11 +32,11 @@ export async function POST(req, res) {
 
     const tokenMaxAge = parseInt(JWT_EXPIRES_IN) * 60;
     const cookieOptions = {
-      name: "token",
+      name: 'token',
       value: token,
       // httpOnly: true,
-      path: "/",
-      secure: process.env.NODE_ENV !== "development",
+      path: '/',
+      secure: process.env.NODE_ENV !== 'development',
       maxAge: tokenMaxAge,
       // sameSite: "None",
     };
@@ -52,10 +52,10 @@ export async function POST(req, res) {
         token: token,
         headers: {
           // Allow your Angular app's origin (replace with your actual Angular app's URL)
-          "Access-Control-Allow-Origin": process.env.API_URL || "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type",
-          "Access-Control-Allow-Credentials": "true",
+          'Access-Control-Allow-Origin': process.env.API_URL || '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Allow-Credentials': 'true',
         },
       }
     );
@@ -63,12 +63,12 @@ export async function POST(req, res) {
     await Promise.all([
       response.cookies.set(cookieOptions),
       response.cookies.set({
-        name: "logged-in",
-        value: "true",
+        name: 'logged-in',
+        value: 'true',
         maxAge: tokenMaxAge,
 
-        path: "/",
-        secure: process.env.NODE_ENV !== "development",
+        path: '/',
+        secure: process.env.NODE_ENV !== 'development',
         maxAge: tokenMaxAge,
         // sameSite: "None",
       }),
