e25468c314823f774ce6a474e6c09f86299dffd2
HemrajKS <hkshettigar1947@gmail.com>
2023-10-10 15:44:34 +0530

    changes

--- src/app/api/order/deleteOrder/route.js
+++ src/app/api/order/deleteOrder/route.js
@@ -1,11 +1,11 @@
-import { getErrorResponse } from '@/lib/helpers';
-import { logout } from '@/lib/logout';
-import connectDB from '@/lib/mongodb';
-import { verifyJWT } from '@/lib/token';
-import { verifyPass } from '@/lib/verifyPass';
-import Order from '@/models/order';
-import User from '@/models/user';
-import { NextResponse, NextRequest } from 'next/server';
+import { getErrorResponse } from "@/lib/helpers";
+import { logout } from "@/lib/logout";
+import connectDB from "@/lib/mongodb";
+import { verifyJWT } from "@/lib/token";
+import { verifyPass } from "@/lib/verifyPass";
+import Order from "@/models/order";
+import User from "@/models/user";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(req) {
   const body = await req.json();
@@ -13,7 +13,7 @@ export async function POST(req) {
     await connectDB();
     // const userId = req.headers.get('X-User-Id');
 
-    let token = req.cookies.get('token')?.value;
+    let token = req.cookies.get("token")?.value;
     const userId = (await verifyJWT(token)).sub;
 
     const user = await User.findOne({ _id: userId });
@@ -33,11 +33,11 @@ export async function POST(req) {
         let json_response = {
           status: true,
           categories: order,
-          message: 'Order deleted successfully',
+          message: "Order deleted successfully",
         };
         return NextResponse.json(json_response);
       } else {
-        return getErrorResponse(403, 'Only admins can delete order ');
+        return getErrorResponse(403, "Only admins can delete order ");
       }
     } else {
       return logout();
@@ -45,13 +45,13 @@ export async function POST(req) {
   } catch (error) {
     let json_response = {
       status: false,
-      results: 'some error occured',
+      results: "some error occured",
       error: error,
     };
     return NextResponse.json(json_response, {
       status: 500,
       headers: {
-        'Access-Control-Allow-Methods': 'POST',
+        "Access-Control-Allow-Methods": "POST",
       },
     });
   }
