af4c054c140de96ae5172bc50580b8d149ad86d1
HemrajKS <hkshettigar1947@gmail.com>
2023-10-10 14:44:36 +0530

    changes

--- src/app/(Frontend)/orders/layout.js
+++ src/app/(Frontend)/orders/layout.js
@@ -1,3 +1,8 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function Layout({ children }) {
+  const router = useRouter();
+  console.log(router);
   return <>{children}</>;
 }
