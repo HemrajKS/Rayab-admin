c2ea2e81746dac4cbe4eea6bb155b2c0671c44c6
HemrajKS <hkshettigar1947@gmail.com>
2023-10-13 10:59:02 +0530

    changes

--- src/middleware.js
+++ src/middleware.js
@@ -12,10 +12,7 @@ export const corsHeaders = {
 
 export async function middleware(req) {
   if (req.method === "OPTIONS") {
-    return NextResponse.json(
-      { message: "this causing error" },
-      { headers: corsHeaders }
-    );
+    return NextResponse.json({}, { headers: corsHeaders });
   }
   let token;
 
