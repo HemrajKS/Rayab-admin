2a59e59ff78f0f161a11503eb465e3df883db49c
HemrajKS <hkshettigar1947@gmail.com>
2023-10-18 16:06:13 +0530

    changes

--- src/app/api/order/orderCompleted/route.js
+++ src/app/api/order/orderCompleted/route.js
@@ -1,28 +1,28 @@
-import { sendMail } from '@/app/services/mail';
-import { getErrorResponse } from '@/lib/helpers';
-import { logout } from '@/lib/logout';
-import connectDB from '@/lib/mongodb';
-import { verifyJWT } from '@/lib/token';
-import { verifyPass } from '@/lib/verifyPass';
-import Order from '@/models/order';
-import Product from '@/models/product';
-import User from '@/models/user';
-import UserModel from '@/models/user';
-import { NextResponse, NextRequest } from 'next/server';
+import { sendMail } from "@/app/services/mail";
+import { getErrorResponse } from "@/lib/helpers";
+import { logout } from "@/lib/logout";
+import connectDB from "@/lib/mongodb";
+import { verifyJWT } from "@/lib/token";
+import { verifyPass } from "@/lib/verifyPass";
+import Order from "@/models/order";
+import Product from "@/models/product";
+import User from "@/models/user";
+import UserModel from "@/models/user";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(req) {
   const body = await req.json();
   // const userId = req.headers.get('X-User-Id');
 
   let token;
-  if (req.cookies.has('token')) {
-    token = req.cookies.get('token')?.value;
-  } else if (req.headers.get('Authorization')?.startsWith('Bearer ')) {
-    token = req.headers.get('Authorization')?.substring(7);
+  if (req.cookies.has("token")) {
+    token = req.cookies.get("token")?.value;
+  } else if (req.headers.get("Authorization")?.startsWith("Bearer ")) {
+    token = req.headers.get("Authorization")?.substring(7);
   } else {
     return getErrorResponse(
       401,
-      'You are not loggen in, Please log in to proceed...'
+      "You are not loggen in, Please log in to proceed..."
     );
   }
   const userId = (await verifyJWT(token))?.sub;
@@ -37,8 +37,8 @@ export async function POST(req) {
 
     if (pass) {
       if (user.isAdmin) {
-        if (order.products && JSON.stringify(order.products) !== '[]') {
-          if (body.status === 'completed') {
+        if (order.products && JSON.stringify(order.products) !== "[]") {
+          if (body.status === "completed") {
             for (const orderProduct of order.products) {
               const quantityToDecrement = -orderProduct.quantity;
               await Product.findOneAndUpdate(
@@ -50,7 +50,7 @@ export async function POST(req) {
             }
             orderNew = await Order.findOneAndUpdate(
               { _id: body.id },
-              { status: 'completed' },
+              { status: "completed" },
               { new: true }
             );
 
@@ -59,26 +59,39 @@ export async function POST(req) {
               productStatus: `Completed`,
               orderNumber: body.id,
               link: `${process.env.API_URL}/order/${body.id}`,
-              template: 'update',
+              template: "update",
+            });
+
+            const convertedProducts = order?.products?.map((product) => {
+              return {
+                link: `${process.env.API_URL}/reviews/${product.product}`,
+              };
+            });
+
+            await sendMail({
+              to: order.email,
+              orderId: body.id,
+              reviewLinks: convertedProducts,
+              template: "review",
             });
 
             json_response = {
               status: true,
               data: orderNew,
-              message: 'Order Completed Successfully',
+              message: "Order Completed Successfully",
             };
 
             return NextResponse.json(json_response);
-          } else if (body.status === 'rejected') {
+          } else if (body.status === "rejected") {
             orderNew = await Order.findOneAndUpdate(
               { _id: body.id },
-              { status: 'rejected' },
+              { status: "rejected" },
               { new: true }
             );
             json_response = {
               status: true,
               data: orderNew,
-              message: 'Order Rejected Successfully',
+              message: "Order Rejected Successfully",
             };
 
             await sendMail({
@@ -86,21 +99,21 @@ export async function POST(req) {
               productStatus: `Rejected`,
               orderNumber: body.id,
               link: `${process.env.API_URL}/order/${body.id}`,
-              template: 'update',
+              template: "update",
             });
 
             return NextResponse.json(json_response);
-          } else if (body.status === 'pending') {
+          } else if (body.status === "pending") {
             orderNew = await Order.findOneAndUpdate(
               { _id: body.id },
-              { status: 'pending' },
+              { status: "pending" },
               { new: true }
             );
 
             json_response = {
               status: true,
               data: orderNew,
-              message: 'Order status changed to Pending',
+              message: "Order status changed to Pending",
             };
 
             await sendMail({
@@ -108,14 +121,14 @@ export async function POST(req) {
               productStatus: `Pending`,
               orderNumber: body.id,
               link: `${process.env.API_URL}/order/${body.id}`,
-              template: 'update',
+              template: "update",
             });
             return NextResponse.json(json_response);
           } else {
-            return getErrorResponse(404, 'Orders not found');
+            return getErrorResponse(404, "Orders not found");
           }
         } else {
-          return getErrorResponse(406, 'Only admins can change order status');
+          return getErrorResponse(406, "Only admins can change order status");
         }
       }
     } else {
@@ -123,17 +136,17 @@ export async function POST(req) {
     }
   } catch (error) {
     if (error.code === 11000) {
-      return getErrorResponse(406, 'Duplicate entries');
+      return getErrorResponse(406, "Duplicate entries");
     }
     let json_response = {
       status: false,
-      results: 'some error occured',
+      results: "some error occured",
       error: error,
     };
     return NextResponse.json(json_response, {
       status: 500,
       headers: {
-        'Access-Control-Allow-Methods': 'GET',
+        "Access-Control-Allow-Methods": "GET",
       },
     });
   }
