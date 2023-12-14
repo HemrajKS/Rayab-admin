e879f0a710f840fa5275e69b15d29e4c05915c37
HemarajKS <hemaraja.shettigar@robosoftin.com>
2023-12-14 09:11:35 +0530

    changes

--- src/models/product.js
+++ src/models/product.js
@@ -47,12 +47,18 @@ const productSchema = new Schema(
       {
         user: {
           type: String,
+          required: function () {
+            return this.reviews.length > 0;
+          },
         },
         rating: Number,
         comment: String,
         userId: {
           type: String,
           unique: true,
+          required: function () {
+            return this.reviews.length > 0;
+          },
         },
       },
     ],
