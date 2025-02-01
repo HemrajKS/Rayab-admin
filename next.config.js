4d83f5588be1cc7da68ca5ba3e69d4e99c48efd4
hemaraja.shettigar <hemaraja.shettigar@robosoftin.com>
2025-02-01 21:07:44 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
   images: {
     domains: ["images.unsplash.com", "res.cloudinary.com"],
   },
